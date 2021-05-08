6f7e195c383d3e0f37a2a0bd0d727276b8ce9e8d
Glemhel <ya.rudakov2001@yandex.ru>
2021-05-08 20:07:40 +0300

    refactor: renamed script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+gcc -pthread -o app application.c
+gnome-terminal -e "./app 0 1" &
+gnome-terminal -e "./app 1 0" &
+gnome-terminal -e "./app 2 0" &
+gnome-terminal -e "./app 3 0" &
+gnome-terminal -e "./app 4 0" &
+#for i in {1..1}
+#do
+#    gnome-terminal -e ./peer$i $i 0
+#done
